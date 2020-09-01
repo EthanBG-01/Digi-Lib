618b0cda3a44c0d7a7f9dae8dbbabe3a7a74d325
EthanDevs404 <63881996+EthanDevs404@users.noreply.github.com>
2020-09-01 13:45:58 +0100

    Added the registration route for users

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -11,4 +11,49 @@ router.get("/test", (req, res) => {
   res.send("Test Successful");
 });
 
+//Asnyc as it contacts the database:
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, confirmPassword } = req.body;
+    if (!name || !email || !password)
+      res.status(400).json({ ErrorMsg: "Not all fields have been entered." });
+
+    //Check if the email has been used before
+    const existing = await User.findOne({ email: email });
+    if (existing)
+      res
+        .status(400)
+        .json({ ErrorMsg: "A user with this email already exists." });
+
+    if (password.length < 5)
+      res.status(400).json({
+        ErrorMsg: "Password too short. Should contain at least 5 characters.",
+      });
+
+    if (password != confirmPassword)
+      res.status(400).json({ ErrorMsg: "Passwords don't match." });
+
+    //Hashing password (MUST not be in plaintext.)
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(password, salt);
+
+    //Construct a user model object:
+    const user = new User({
+      name,
+      email,
+      password: hash,
+    });
+
+    //Saves to mongoDB database:
+    const saved = await user.save();
+
+    //returns the saved user:
+    res.json(saved);
+  } catch (err) {
+    //Useful for debugging.
+    console.log("There has been an error: " + err);
+    res.status(500).json({ error: err.nessage });
+  }
+});
+
 module.exports = router;
